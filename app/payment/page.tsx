d10220357eb15d178abc451827c7676651cd8875
Anas MESRI <266374851+nxtex@users.noreply.github.com>
2026-03-25 22:23:01 +0100

    Comment out BubbleFooter component
    Comment out the BubbleFooter component in PaymentPage.

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,11 +1,11 @@
 import PaymentCard from '@/components/PaymentCard';
-import BubbleFooter from '@/components/BubbleFooter';
+// import BubbleFooter from '@/components/BubbleFooter';
 
 export default function PaymentPage() {
   return (
     <>
       <PaymentCard />
-      <BubbleFooter />
+      {/* <BubbleFooter /> */}
     </>
   );
 }
